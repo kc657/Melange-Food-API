d7d2085eae9756a578a5f4b1361fbd1a391b530c
kc657 <kvn.0218@gmail.com>
2017-07-24 16:46:07 -0700

    Switched API Provider in app.js file, api key already in HTML file

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,8 @@
 function index (req, res) {
   res.json({
-    message: 'Hello'
+    message: 'Welcome to our food searching API!',
+    creators: 'Kevin & Mary',
+    github: 'https://github.com/kc657/food-search-api'
   })
 }
 
