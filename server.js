d5c80feb1f278fde8a2d74b50a64dd4922eea93b
kc657 <kvn.0218@gmail.com>
2017-07-24 14:11:40 -0700

    Server.js setup complete and running on localhost:3000

---
+++ server.js
@@ -0,0 +1,25 @@
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+// serve static :files from public folder
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({extended: true}))
+
+const controllers = require('./controllers')
+
+// database
+const db = require('./models')
+
+// routes
+app.get('/', function homepage (req, res) {
+  res.sendFile('views/index.html', ({root: __dirname}))
+})
+
+/**********
+ * SERVER *
+ **********/
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is up and running on http://localhost:3000/')
+})
