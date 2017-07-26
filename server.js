464d386f3b0cb541782c0dc985b205a32e3c2f78
Kevin <kvn.0218@gmail.com>
2017-07-25 18:57:35 -0700

    hello

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-
+ 
 
 // serve static :files from public folder
 app.use(express.static(__dirname + '/public'))
