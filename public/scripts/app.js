a0f6180fa4a65afeeca55cb178cc9a3842de76a1
kc657 <kvn.0218@gmail.com>
2017-07-29 15:29:46 -0700

    Search modal does not store into database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -38,6 +38,9 @@ $(document).ready(function () {
   // save review modal save button
   $('#saveReview').on('click', handleNewReviewSubmit)
 
+  // search modal save recipe button
+  // $('#add-recipe').on('click', saveRecipeFromModal)
+
   // edit recipe click to pop modal
   $('#recipes').on('click', '.edit-recipe', function handleEditRecipeClick (e) {
     const recipeId = $(this).closest('.recipe').data('recipe-id')
@@ -98,29 +101,74 @@ function getRecipes () {
     url: 'https://api.edamam.com/search',
     data: $('form').serialize(),
     dataType: 'json',
-    success: function postRecipes (recipes) {
-      let edamamApiRecipe = {
-        name: recipes.hits[0].recipe.label,
-        calories: recipes.hits[0].recipe.calories,
-        healthLabels: recipes.hits[0].recipe.healthLabels,
-        source: recipes.hits[0].recipe.source,
-        sourceUrl: recipes.hits[0].recipe.url,
-        imgUrl: recipes.hits[0].recipe.image,
-        ingredients: recipes.hits[0].recipe.ingredientLines,
-        yield: recipes.hits[0].recipe.yield
-      }
-      $.ajax({
-        method: 'POST',
-        url: '/api/recipes',
-        data: edamamApiRecipe,
-        success: renderModalSearchRecipe,
-        error: function () {
-          console.log('Recipe posting failed')
-        }
-      })
-    },
+    success: renderModalSearchRecipe,
     error: function getApiRecipesError () {
       console.log('Get Recipes Error')
+    }
+  })
+}
+
+// takes api data and puts on modal
+function renderModalSearchRecipe (recipes) {
+  let edamamApiRecipe = {
+    name: recipes.hits[0].recipe.label,
+    calories: recipes.hits[0].recipe.calories,
+    healthLabels: recipes.hits[0].recipe.healthLabels,
+    source: recipes.hits[0].recipe.source,
+    sourceUrl: recipes.hits[0].recipe.url,
+    imgUrl: recipes.hits[0].recipe.image,
+    ingredients: recipes.hits[0].recipe.ingredientLines,
+    yield: recipes.hits[0].recipe.yield
+  }
+  let ingredientsFormattedList = renderIngredient(edamamApiRecipe.ingredients)
+  let recipeHtml = (`
+    <div class='modal modal-transparent fade' tabindex='-1' role='dialog' id='recipeModal'>
+    <div class='modal-dialog'>
+    <div class='recipe' data-recipe-id=''>
+    <div class='col-md-12'>
+    <div class='thumbnail'>
+    <img src='${edamamApiRecipe.imgUrl}' alt='recipe image'>
+    <div class='caption'>
+    <h4 class='inline-header'><strong>${edamamApiRecipe.name}</strong></h4>
+    <p>via<a href='${edamamApiRecipe.sourceUrl}'> ${edamamApiRecipe.source}</a></p>
+    <h4 class='inline-header'><strong>Ingredients:</strong></h4>
+    <ul>${ingredientsFormattedList}</ul>
+    <h4 class='inline-header'><strong>Yield:</strong></h4>
+    <ul>${edamamApiRecipe.yield}</ul>
+    <div class='bottom-align-buttons'>
+    <button type='button' class='btn btn-primary add-recipe'><span class='icon'><i class='fa fa-plus'></i></span> Add Recipe</button>
+    <button type='button' class='btn btn-danger delete-recipe'><span class='icon'><i class='fa fa-trash-o'></i></span> Delete Recipe</button>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    `)
+  $('#recipes').prepend(recipeHtml)
+  $('#recipeModal').modal()
+}
+
+// post recipes into database
+function postRecipes (recipes) {
+  let edamamApiRecipe = {
+    name: recipes.hits[0].recipe.label,
+    calories: recipes.hits[0].recipe.calories,
+    healthLabels: recipes.hits[0].recipe.healthLabels,
+    source: recipes.hits[0].recipe.source,
+    sourceUrl: recipes.hits[0].recipe.url,
+    imgUrl: recipes.hits[0].recipe.image,
+    ingredients: recipes.hits[0].recipe.ingredientLines,
+    yield: recipes.hits[0].recipe.yield
+  }
+  $.ajax({
+    method: 'POST',
+    url: '/api/recipes',
+    data: edamamApiRecipe,
+    success: renderModalSearchRecipe,
+    error: function () {
+      console.log('Recipe posting failed')
     }
   })
 }
@@ -155,38 +203,6 @@ function renderIngredient (ingredients) {
       `)
   })
   return ingredientHtml
-}
-
-function renderModalSearchRecipe (recipe) {
-  let ingredientsFormattedList = renderIngredient(recipe.ingredients)
-  let recipeHtml = (`
-    <div class='modal modal-transparent fade' tabindex='-1' role='dialog' id='recipeModal'>
-      <div class='modal-dialog'>
-        <div class='recipe' data-recipe-id='${recipe._id}'>
-          <div class='col-md-12'>
-            <div class='thumbnail'>
-              <img src='${recipe.imgUrl}' alt='recipe image'>
-              <div class='caption'>
-                <h4 class='inline-header'><strong>${recipe.name}</strong></h4>
-                <p>via<a href='${recipe.sourceUrl}'> ${recipe.source}</a></p>
-                <h4 class='inline-header'><strong>Ingredients:</strong></h4>
-                <ul>${ingredientsFormattedList}</ul>
-                <h4 class='inline-header'><strong>Reviews:</strong></h4>
-                <ul>${recipe.reviews}</ul>
-                <div class='bottom-align-buttons'>
-                  <button type='button' class='btn btn-primary add-review'><span class='icon'><i class='fa fa-plus'></i></span> Add Review</button>
-                  <button type='button' class='btn btn-info edit-recipe'><span class='icon'><i class='fa fa-pencil'></i></span> Edit</button>
-                  <button type='button' class='btn btn-danger delete-recipe'><span class='icon'><i class='fa fa-trash-o'></i></span> Delete Recipe</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    `)
-  $('#recipes').prepend(recipeHtml)
-  $('#recipeModal').modal()
 }
 
 function renderRecipes (recipe) {
